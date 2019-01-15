923b6c7f7071250cd4a06f93ca3a34ecfbf915e1
MKLUO <kkeenn12345566@gmail.com>
2019-01-15 11:56:45 +0800

    Outlining.

--- NnManager.cs
+++ NnManager.cs
@@ -1,23 +1,66 @@
 using System.Collections.Generic;
 
 namespace NnManager {
-
-    class Template {
-
-        public string generateContent() {
-
-        }
-    }
-
     public class Project {
+
+        class Template {
+
+            public Template(
+                string content
+            ) {
+
+            }
+            public string generateContent() {
+                return "";
+            }
+        }
 
         class Task {
 
-            public Task(
-                string id, 
-                string content) {
+            enum Status {
+                New,
+                Done,
+                Error
+            };
 
+            public Task(
+                string content_) {
+                    content = content_;
+                    status = Status.New;
                 }
+
+            public void Run() {
+                status = Status.Done;
+            }
+
+            readonly string content;
+
+            Status status;
+        }
+
+        public Project(
+            string name_) {
+            name = name_;
+        }
+
+        public void AddTemplate(
+            string id,
+            string content) {
+            templates.Add(
+                id,
+                new Template(content)
+            );
+        }
+
+        public void AddTask(
+            string name,
+            string templateId,
+            string param) {
+            tasks.Add(
+                name,
+                new Task(
+                    templates[templateId].gene)
+            );
         }
 
         public void Load() {
@@ -28,6 +71,9 @@ namespace NnManager {
             
         }
 
-        List<Task>  tasks;
+        readonly string name;
+
+        Dictionary<string, Template>    templates;
+        Dictionary<string, Task>        tasks;
     }
 }
