2783d8daf6cbf749eeb16bf89e41832933ca1059
MKLUO <kkeenn12345566@gmail.com>
2019-01-15 11:11:29 +0800

    first commit

---
+++ NnManager.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace NnManager {
+
+    class Template {
+
+        public string generateContent() {
+
+        }
+    }
+
+    public class Project {
+
+        class Task {
+
+            public Task(
+                string id, 
+                string content) {
+
+                }
+        }
+
+        public void Load() {
+
+        }
+
+        public void Save() {
+            
+        }
+
+        List<Task>  tasks;
+    }
+}
