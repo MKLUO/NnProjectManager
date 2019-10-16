30342ae4a08b7feaf22595a67a74d9318e050147
MKLUO <r06222036@ntu.edu.tw>
2019-10-16 20:34:03 +0800

    1st commit

---
+++ OLD/Modules/NnDQDJ2.cs
@@ -0,0 +1,53 @@
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Threading;
+
+namespace NnManager {
+    using RPath = Util.RestrictedPath;
+
+    partial class NnTask {
+
+        RPath NnDQDJ2Path => FSPath.SubPath("NnDQDJ2");
+        RPath NnDQDJ2ResultPath => NnDQDJPath.SubPath($"_Result.txt");
+        RPath NnDQDJ2ReportPath => NnDQDJPath.SubPath($"_Report.txt");
+
+        NnModule NnDQDJ2(ImmutableDictionary<string, string> options) =>
+            new NnModule(
+                "NN DQD J 2",
+                NnDQDJ2CanExecute,
+                NnDQDJ2IsDone,
+                NnDQDJ2Execute,
+                NnDQDJ2GetResult,
+                NnDQDJ2DefaultOption,
+                options);
+
+        public static ImmutableDictionary<string, string> NnDQDJ2DefaultOption =>
+            new Dictionary<string, string> { 
+            }.ToImmutableDictionary();
+
+        bool NnDQDJ2CanExecute() => NnDQDReportIsDone();
+
+        bool NnDQDJ2IsDone() => File.Exists(NnDQDJ2ResultPath);
+
+        string NnDQDJ2GetResult() => File.ReadAllText(NnDQDJ2ResultPath);
+
+        bool NnDQDJ2Execute(CancellationToken ct, ImmutableDictionary<string, string> options) {
+            
+            /*
+                NOTE: After DQDReport is done,
+                    single-particle eigenbasis from NN should be directly accessible
+                    from an unified interface.
+
+                NOTE: Basis other than NN-solved s.p. basis should be pluggable here.
+             */
+
+            /// NOTE: Evaluate coulomb kernel (from a ref. WF)
+
+            /// 
+
+            return true;
+        }
+    }    
+}
